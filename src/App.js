08eab2b8b972e5c5be2a5e0c8428f735c9b864a5
KevinThomas441 <kevinthomas441@gmail.com>
2020-09-23 00:07:59 -0400

    Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import BaseLayout from "./canvas/layout/baselayout";
+import NavBar from "./canvas/navbar";
 
 class PortfolioApp extends React.Component {
   render(){
     return (
-      <React.Fragment>Hello World</React.Fragment>
+    <BaseLayout navigation={ <NavBar /> } footer="Footer" >
+      Content
+    </BaseLayout>
     )
   }
 }
--- src/canvas/layout/baselayout.js
+++ src/canvas/layout/baselayout.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, makeStyles, CssBaseline } from '@material-ui/core';
 
-const useStyle = makeStyle(theme => {
+const useStyle = makeStyles(theme =>({
     root: {flexGrow: 1}
-})
+}));
 
-const BaseLayout = () => {
-    return <Grid container></Grid>
+const BaseLayout = ({ navigation, children, footer }) => {
+    return <React.Fragment>
+        <CssBaseline />
+        <Grid container>
+            <Grid item>{navigation}</Grid>
+            <Grid item>{children}</Grid>
+            <Grid item>{footer}</Grid>
+        </Grid>
+    </React.Fragment>
 }
+
+export default BaseLayout;
