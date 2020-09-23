c72fc37e39e924fed061e25a344d7d5b2c3f28ff
KevinThomas441 <kevinthomas441@gmail.com>
2020-09-23 01:01:28 -0400

    Added Navbar

---
+++ src/canvas/navbar/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Avatar, Toolbar, makeStyles, Typography } from '@material-ui/core';
+import NavLink from './navlink';
+
+const useStyle = makeStyles((theme) =>({
+    root: {},
+    title: {
+        marginLeft: theme.spacing(1)
+    },
+    fillSpace: {
+        flexGrow:1
+    },
+    small: {
+        width: theme.spacing(3),
+        height: theme.spacing(3),
+        marginLeft: theme.spacing(3)
+    }
+})
+);
+
+const NavBar = () => {
+    const classes = useStyle();
+    return (
+    <Toolbar>
+        <Avatar alt="Kevin Thomas" src = "/assets/kevin.png" />
+        <Typography classes = {{root:classes.title}}>
+            Kevin Thomas
+        </Typography>
+        <div className = {classes.fillSpace}></div>
+        <NavLink url="#" label="about" active/>
+        <NavLink url="#" label="projects"/>
+        <NavLink url="#" label="skills"/>
+        <NavLink url="#" label="experience"/>
+        <Avatar alt="LinkedIn" src = "/assets/kevin.png" className={classes.small}/>
+        <Avatar alt="Github" src = "/assets/kevin.png" className={classes.small}/>
+        <Avatar alt="Email" src = "/assets/kevin.png" className={classes.small}/>
+    </Toolbar>
+    )
+}
+
+export default NavBar
