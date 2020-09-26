0c2337607e11aa88dd42c988342db190429537e3
KevinThomas441 <kevinthomas441@gmail.com>
2020-09-26 13:09:37 -0400

    Add skills

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import React from 'react';
 import BaseLayout from './canvas/layout/baselayout';
 import NavBar from './canvas/navbar';
 import Header from './canvas/header';
+import Projects from './canvas/projects';
+import Skills from './canvas/skills';
 
 function PortfolioApp() {
   return (
     <BaseLayout navigation = {<NavBar />}>
       <Header title="Kevin Thomas" caption="Software Development | Machine Learning | Data science"/>
+      <Projects />
+      <Skills />
     </BaseLayout>
   );
   
